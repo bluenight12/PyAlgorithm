bf980829cb3c67dd54db693f7cfe0675b9537f77
bluenight12 <ekehd258@gmail.com>
2024-02-27 19:47:40 +0900

    프로그래머스 짝지어 제거하기

---
+++ Stage4/couple_delete.py
@@ -0,0 +1,46 @@
+def solution(s):
+    
+    l = ['',s[0]]
+    for i in s[1:]:
+        if l[-1] != i:
+            l.append(i)
+        else:
+            l.pop()
+    answer = 1 if len(l) == 1 else 0
+    return answer
+    
+    '''
+    answer = -1
+    s = list(s)
+    n = 0
+    start, end = 0, 1
+    tmp = []
+    # 0 1 2 3 4 5 
+    while end < len(s):
+        if start < 0 or s[start] == ' ':
+            start = end
+            end += 1
+        if end >= len(s):
+            break
+        if s[start] == s[end]:
+            n += 2
+            s[start] = ' '
+            s[end] = ' '
+            start -= 1
+            if tmp:
+                start = tmp[-1]
+                tmp.pop(-1)
+            end += 1
+        else:
+            tmp.append(start)
+            start = end
+            end += 1
+        
+            
+    answer = 1 if n == len(s) else 0
+    return answer
+    '''
+
+
+stn = 'azzbxxccddsszzvvaawweerrabccbaba'
+print(solution(stn))
