ad796e7a64e75d8df0edfff94891b72cb92f6f20
bluenight12 <ekehd258@gmail.com>
2024-03-02 19:02:54 +0900

    divmod & int(str, n)

--- Stage4/base3_reverse.py
+++ Stage4/base3_reverse.py
@@ -5,13 +5,17 @@ def solution(n):
     answer = 0
     base = []
     while n != 0:
-        div = n % 3
-        n = n // 3
-        base.insert(0, div)
-    print(base)
+        #div와 mod를 따로 나누지 않아도 됨
+        n, div = divmod(n, 3)
+        base.append(div)
+        #base.insert(0, div)
+
+    """
     for i in range(0, len(base)):
         answer += base[i] * (3**i)
-    return answer
+    """
+    #int(string, x진법에 해당하는 x)를 넣으면 알아서 숫자로 변환해줌
+    return int(''.join(map(str, base)), 3)
 
 
 print(solution(45))
